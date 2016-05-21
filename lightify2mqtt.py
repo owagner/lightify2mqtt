a0cc6c27a43a609457fd5713affef7e455014365
Oliver Wagner <owagner@tellerulam.com>
2016-05-21 12:47:45 +0200

    - working group setting support

--- lightify2mqtt.py
+++ lightify2mqtt.py
@@ -9,7 +9,7 @@
 #
 # Requires:
 # - Eclipse Paho for Python - http://www.eclipse.org/paho/clients/python/
-# - requests 2.10.0 -- do manually upgrade using pip!
+# - requests 2.10.0 -- do manually upgrade using pip if your system has an old one!
 #
 
 import argparse
@@ -25,7 +25,7 @@ import traceback
 import requests
 import json
 
-version="0.1"
+version="0.2"
     
 parser = argparse.ArgumentParser(description='Bridge between an OSRAM Lightify Gateway and MQTT')
 parser.add_argument('--mqtt-host', default='localhost', help='MQTT server address. Defaults to "localhost"')
@@ -35,7 +35,7 @@ parser.add_argument('--clientid', default='modbus2mqtt', help='Client ID prefix 
 parser.add_argument('--user', required=True, help='Lightify Username')
 parser.add_argument('--password', required=True, help='Lightify Password')
 parser.add_argument('--serial', required=True, help='Lightify Gateway Serial (printed on device, WITHOUT suffix)')
-parser.add_argument('--pollfreq', type=int, default=30, help='Polling interval in seconds. Defaults to 30')
+parser.add_argument('--pollfreq', type=int, default=42, help='Polling interval in seconds. Defaults to 42')
 parser.add_argument('--log', help='set log level to the specified value. Defaults to WARNING. Use DEBUG for maximum detail')
 parser.add_argument('--syslog', action='store_true', help='enable logging to syslog')
 args=parser.parse_args()
@@ -53,44 +53,73 @@ if not topic.endswith("/"):
 
 devices={}
 devicesByName={}
+groupsByName={}
 wakeup=False
 
 logging.info('Starting lightify2mqtt V%s with topic prefix \"%s\"' %(version, topic))
 
-def handleSetLights(name,msg):
-	global devices,devicesByName
-	# Find lamp
-	dev=None
-	parms={}
-	if not name=="all":
-		dev=devicesByName[name]
-	try:
-		val=float(msg.payload)
-		if val==0:
-			parms={"onoff":0}
-		else:
-			parms={"onoff":1,"level":val}
-	except:
-		# Seems to be complex type, try JSON
-		parms=json.loads(msg.payload.decode("utf-8"))
-	if dev==None:
-		doRequest("device/all/set",parms)
-	else:
-		parms["idx"]=dev["deviceId"]
-		doRequest("device/set",parms)
+def handleSetLights(name,msg,datapoint):
+    global devices,devicesByName
+    # Find lamp
+    dev=None
+    parms={}
+    if not name=="all":
+        dev=devicesByName[name]
+    try:
+        val=float(msg.payload)
+        if datapoint is not None:
+            parms[datapoint]=val
+        elif val==0:
+            parms={"onoff":0}
+        else:
+            parms={"onoff":1,"level":val}
+    except:
+        # Seems to be complex type, try JSON
+        parms=json.loads(msg.payload.decode("utf-8"))
+    if dev==None:
+        doRequest("device/all/set",parms)
+    else:
+        parms["idx"]=dev["deviceId"]
+        doRequest("device/set",parms)
 
-def handleSet(typeid,name,msg):
-	if typeid=="lights":
-		handleSetLights(name,msg)
+def handleSetGroups(name,msg,datapoint):
+    global groupsByName
+    parms={}
+    # Find group
+    group=groupsByName[name]
+    try:
+        val=float(msg.payload)
+        if datapoint is not None:
+            parms[datapoint]=val
+        elif val==0:
+            parms={"onoff":0}
+        else:
+            parms={"onoff":1,"level":val}
+    except:
+        # Seems to be complex type, try JSON
+        parms=json.loads(msg.payload.decode("utf-8"))
+    parms["idx"]=group["groupId"]
+    doRequest("group/set",parms)
+
+def handleSet(typeid,name,msg,datapoint):
+    if typeid=="lights":
+        handleSetLights(name,msg,datapoint)
+    elif typeid=="groups":
+        handleSetGroups(name,msg,datapoint)
 
 def messagehandler(mqc,userdata,msg):
     global wakeup
     try:
-        (cmd,typeid,name) = msg.topic[len(topic):].split("/")
+        args=msg.topic[len(topic):].split("/",4)
+        if len(args)==4:
+            (cmd,typeid,name,datapoint)=args
+        else:
+            (cmd,typeid,name)=args
+            datapoint=None
         if cmd=="set":
-        	handleSet(typeid,name,msg)
-        	# Immediately wake up the polling loop
-        	wakeup=True
+            handleSet(typeid,name,msg,datapoint)
+            # Immediately wake up the polling loop
+            wakeup=True
         
     except Exception as e:
         logging.error("Error on message " + msg.topic + " :" + str(e))
@@ -106,74 +135,80 @@ def disconnecthandler(mqc,userdata,rc):
     logging.warning("Disconnected from MQTT broker with rc=%d" % (rc))
 
 def genEndpoint(ep):
-	return "https://eu.lightify-api.org/lightify/services/"+ep;
+    return "https://eu.lightify-api.org/lightify/services/"+ep;
 
 def doRequest(ep,params=None):
-	return requests.get(genEndpoint(ep),headers={
-		"authorization": authtoken	
-	},params=params)
+    return requests.get(genEndpoint(ep),headers={
+        "authorization": authtoken  
+    },params=params)
 
 def publishDevice(dev):
-	if dev["on"]:
-		val=dev["brightnessLevel"]
-	else:
-		val=0
-	mqc.publish(topic+"status/lights/"+dev["name"],json.dumps({
-		"val": val,
-		"lightify_state": dev
-	}),qos=1,retain=True)
+    if dev["on"]:
+        val=dev["brightnessLevel"]
+    else:
+        val=0
+    mqc.publish(topic+"status/lights/"+dev["name"],json.dumps({
+        "val": val,
+        "lightify_state": dev
+    }),qos=1,retain=True)
 
 def handleDevice(dev):
-	global devices,devicesByName
-	devid=dev["deviceId"]
-	publish=True
-	if devid in devices:
-		olddev=devices[devid]
-		# Any differences?
-		if dev==olddev:
-			publish=False
-	devices[devid]=dev
-	devicesByName[dev["name"]]=dev
-	if publish:
-		publishDevice(dev)
+    global devices,devicesByName
+    devid=dev["deviceId"]
+    publish=True
+    if devid in devices:
+        olddev=devices[devid]
+        # Any differences?
+        if dev==olddev:
+            publish=False
+    devices[devid]=dev
+    devicesByName[dev["name"]]=dev
+    if publish:
+        publishDevice(dev)
 
 def pollDevices():
-	r=doRequest("devices")
-	if r.status_code==200:
-		for dev in r.json():
-			if dev["deviceType"]=="LIGHT":
-				handleDevice(dev)
-	return
-	
+    r=doRequest("devices")
+    if r.status_code==200:
+        for dev in r.json():
+            if dev["deviceType"]=="LIGHT":
+                handleDevice(dev)
+    return
+    
+def handleGroup(group):
+    global groupsByName
+    groupsByName[group["name"]]=group
+
 def pollGroups():
-	r=doRequest("groups")
-	print(r.json())
-	return
+    r=doRequest("groups")
+    if r.status_code==200:
+        for group in r.json():
+            handleGroup(group)
+    return
 
 def doLightify():
-	global authtoken,wakeup
-	# First, login
-	r=requests.get(genEndpoint("version"))
-	logging.info("The remote gateway uses API version "+r.json()["apiversion"])
-	r=requests.post(genEndpoint("session"),json={
-		"username": args.user,
-		"password": args.password,
-		"serialNumber": args.serial
-	})
-	if(r.status_code>=400 and r.status_code<=499):
-		logging.error("Authorization failed, check credentials! "+str(r.status_code)+" "+r.reason+" "+str(r.json()))
-		sys.exit(1)
-	authtoken=r.json()["securityToken"]
-	logging.info("Successfully logged into remote gateway and obtained auth token for userID "+r.json()["userId"])
-	mqc.publish(topic+"connected",2,qos=1,retain=True)
-	while True:
-		pollDevices()
-		#pollGroups()
-		for i in range(args.pollfreq):
-			time.sleep(1)
-			if wakeup:
-				wakeup=False
-				break
+    global authtoken,wakeup
+    # First, login
+    r=requests.get(genEndpoint("version"))
+    logging.info("The remote gateway uses API version "+r.json()["apiversion"])
+    r=requests.post(genEndpoint("session"),json={
+        "username": args.user,
+        "password": args.password,
+        "serialNumber": args.serial
+    })
+    if(r.status_code>=400 and r.status_code<=499):
+        logging.error("Authorization failed, check credentials! "+str(r.status_code)+" "+r.reason+" "+str(r.json()))
+        sys.exit(1)
+    authtoken=r.json()["securityToken"]
+    logging.info("Successfully logged into remote gateway and obtained auth token for userID "+r.json()["userId"])
+    mqc.publish(topic+"connected",2,qos=1,retain=True)
+    while True:
+        pollDevices()
+        pollGroups()
+        for i in range(args.pollfreq):
+            time.sleep(1)
+            if wakeup:
+                wakeup=False
+                break
 
 try:
     clientid=args.clientid + "-" + str(time.time())
@@ -186,8 +221,8 @@ try:
     mqc.loop_start()
     
     while True:
-    	doLightify()
-    	mqc.publish(topic+"connected",1,qos=1,retain=True)
+        doLightify()
+        mqc.publish(topic+"connected",1,qos=1,retain=True)
       
 except Exception as e:
     logging.error("Unhandled error [" + str(e) + "]")
